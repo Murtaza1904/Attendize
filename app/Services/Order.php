98780c5cad71447d456258c912a866086dd98e5a
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-05-26 02:46:57 +0500

    Update Order.php

--- app/Services/Order.php
+++ app/Services/Order.php
@@ -97,7 +97,7 @@ class Order
 
         // return money($this->taxAmount, $this->event->currency);
         $tax = $this->event->regionTax->tax_type == 'fixed' ? $this->event->regionTax->tax : ($this->orderTotal/100) * $this->event->regionTax->tax;
-       return $this->event->regionTax->tax_type;
+       return ($this->orderTotal/100) * $this->event->regionTax->tax;
         if ($currencyFormatted == false) {
             if ($this->event->regionTax->tax_type == 'fixed') {
                 return number_format($tax, 2, '.', '');
