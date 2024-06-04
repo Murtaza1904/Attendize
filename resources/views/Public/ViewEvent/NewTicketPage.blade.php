91abae326424864fa07813dda9c0f7b7673232d3
Muhammad Murtaza <muhammadmurtaza1904@gmail.com>
2024-06-04 20:57:49 +0500

    sale pause ticket hide

--- resources/views/Public/ViewEvent/NewTicketPage.blade.php
+++ resources/views/Public/ViewEvent/NewTicketPage.blade.php
@@ -106,7 +106,7 @@
                 </div>
             @else
         
-                @if($tickets->count() > 0)
+                @if($tickets->where('is_hidden', false)->where('is_paused', false)->count() > 0)
         
                     {!! Form::open(['url' => route('postValidateTickets', ['event_id' => $event->id]), 'class' => 'ajax']) !!}
                     <div class="row">
@@ -115,14 +115,14 @@
                                 <div class="tickets_table_wrap">
                                     <table class="table">
                                         <?php
-                                        $is_free_event = true;
+                                            $is_free_event = true;
                                         ?>
-                                        @foreach($tickets->where('is_hidden', false) as $ticket)
+                                        @foreach($tickets->where('is_hidden', false)->where('is_paused', false) as $ticket)
                                             <tr class="ticket" property="offers" typeof="Offer">
                                                 <td>
-                                        <span class="ticket-title semibold" property="name">
-                                            {{$ticket->title}}
-                                        </span>
+                                                    <span class="ticket-title semibold" property="name">
+                                                        {{$ticket->title}}
+                                                    </span>
                                                     <p class="ticket-descripton mb0 text-muted" property="description">
                                                         {{$ticket->description}}
                                                     </p>
@@ -136,57 +136,36 @@
                                                             <?php
                                                             $is_free_event = false;
                                                             ?>
-                                                            {{-- <span title='{{money($ticket->price, $event->currency)}} @lang("Public_ViewEvent.ticket_price") + {{money($ticket->total_booking_fee, $event->currency)}} @lang("Public_ViewEvent.booking_fees")'>{{money($ticket->total_price, $event->currency)}} 
-                                                            </span> --}}
                                                             <span title='{{money($ticket->price, $event->currency)}} @lang("Public_ViewEvent.ticket_price") + @lang("Public_ViewEvent.booking_fees")'>{{money($ticket->price, $event->currency)}} Ticket Price 
                                                             </span>
                                                             <div> + Booking Fees</div>
                                                             <span class="tax-amount text-muted text-smaller">{{ ($event->organiser->tax_name && $event->organiser->tax_value) ? '(+'.money(($ticket->total_price*($event->organiser->tax_value)/100), $event->currency).' '.$event->organiser->tax_name.')' : '' }}</span>
                                                             <meta property="priceCurrency"
-                                                                  content="{{ $event->currency->code }}">
+                                                                content="{{ $event->currency->code }}">
                                                             <meta property="price"
-                                                                  content="{{ number_format($ticket->price, 2, '.', '') }}">
+                                                                content="{{ number_format($ticket->price, 2, '.', '') }}">
                                                         @endif
                                                     </div>
                                                 </td>
                                                 <td style="width:85px;">
-                                                    @if($ticket->is_paused)
-        
+                                                    @if($ticket->sale_status === config('attendize.ticket_status_sold_out'))
+                                                        <span class="text-danger" property="availability"
+                                                                content="http://schema.org/SoldOut">
+                                                            @lang("Public_ViewEvent.sold_out")
+                                                        </span>
+                                                    @elseif($ticket->sale_status === config('attendize.ticket_status_before_sale_date'))
                                                         <span class="text-danger">
-                                            @lang("Public_ViewEvent.currently_not_on_sale")
-                                        </span>
-        
+                                                            @lang("Public_ViewEvent.sales_have_not_started")
+                                                        </span>
+                                                    @elseif($ticket->sale_status === config('attendize.ticket_status_after_sale_date'))
+                                                        <span class="text-danger">
+                                                            @lang("Public_ViewEvent.sales_have_ended")
+                                                        </span>
                                                     @else
-        
-                                                        @if($ticket->sale_status === config('attendize.ticket_status_sold_out'))
-                                                            <span class="text-danger" property="availability"
-                                                                  content="http://schema.org/SoldOut">
-                                            @lang("Public_ViewEvent.sold_out")
-                                        </span>
-                                                        @elseif($ticket->sale_status === config('attendize.ticket_status_before_sale_date'))
-                                                            <span class="text-danger">
-                                            @lang("Public_ViewEvent.sales_have_not_started")
-                                        </span>
-                                                        @elseif($ticket->sale_status === config('attendize.ticket_status_after_sale_date'))
-                                                            <span class="text-danger">
-                                            @lang("Public_ViewEvent.sales_have_ended")
-                                        </span>
-                                                        @else
-                                                            {!! Form::hidden('tickets[]', $ticket->id) !!}
-                                                            <meta property="availability" content="http://schema.org/InStock">
-                                                            {{-- <select name="ticket_{{$ticket->id}}" class="form-control"
-                                                                    style="text-align: center">
-                                                                @if ($tickets->count() > 1)
-                                                                    <option value="0">0</option>
-                                                                @endif
-                                                                @for($i=$ticket->min_per_person; $i<=$ticket->max_per_person; $i++)
-                                                                    <option value="{{$i}}">{{$i}}</option>
-                                                                @endfor
-                                                            </select> --}}
-                                                            <input type="number" name="ticket_{{$ticket->id}}" id="digitInput" class="form-control" placeholder="Qty" value="0"
-                                                            min="0" required>
-                                                        @endif
-        
+                                                        {!! Form::hidden('tickets[]', $ticket->id) !!}
+                                                        <meta property="availability" content="http://schema.org/InStock">
+                                                        <input type="number" name="ticket_{{$ticket->id}}" id="digitInput" class="form-control" placeholder="Qty" value="0"
+                                                        min="0" required>
                                                     @endif
                                                 </td>
                                             </tr>
@@ -212,11 +191,13 @@
                                             </td>
                                         </tr>
                                         @endif
-                                        <tr>
-                                            <td colspan="3" style="text-align: center">
-                                                @lang("Public_ViewEvent.below_tickets")
-                                            </td>
-                                        </tr>
+                                        @if ($tickets->where('is_hidden', false)->where('is_paused', false)->count() > 0)
+                                            <tr>
+                                                <td colspan="3" style="text-align: center">
+                                                    @lang("Public_ViewEvent.below_tickets")
+                                                </td>
+                                            </tr>
+                                        @endif
                                         <tr class="checkout">
                                             <td colspan="3">
                                                 @if(!$is_free_event)
@@ -230,9 +211,10 @@
                                                             </div>
                                                         @endif
                                                     </div>
-        
                                                 @endif
-                                                {!!Form::submit('Add To Cart', ['class' => 'btn btn-lg btn-event-link pull-right h-black'])!!}
+                                                @if($tickets->where('is_hidden', false)->where('is_paused', false)->count() > 0)
+                                                    {!!Form::submit('Add To Cart', ['class' => 'btn btn-lg btn-event-link pull-right h-black'])!!}
+                                                @endif
                                             </td>
                                         </tr>
                                     </table>
@@ -280,13 +262,11 @@
                         </div>
                     </div>
                 @endif
-        
                 <div class="col-md-{{ $descriptionColSize }}">
                     <div class="content event_details" property="description">
                         {!! md_to_html($event->description) !!}
                     </div>
                 </div>
-        
                 @if ($event->images->count() && $event->event_image_position == 'right')
                     <div class="col-md-5">
                         <div class="content event_poster">
@@ -411,11 +391,9 @@
                                 {{$event->organiser->name}}
                             @endif
                         </h3>
-        
                         <div property="description">
                             {!! md_to_html($event->organiser->about) !!}
                         </div>
-        
                         <p>
                             @if($event->organiser->facebook)
                                 <a property="sameAs" href="https://fb.com/{{$event->organiser->facebook}}" class="btn btn-facebook" target="__blank">
